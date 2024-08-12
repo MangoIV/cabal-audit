a9b5ae70b494bb85a8e13ccded25222399143bf0
Magnus Viernickel <magnus.viernickel@wire.com>
2024-08-12 15:13:13 +0200

    [chore] move to newever typst version

--- nix/haskell-overlay.nix
+++ nix/haskell-overlay.nix
@@ -12,7 +12,7 @@
 
   toml-parser = hprev.toml-parser_2_0_1_0;
   sel = hlib.doJailbreak (hlib.markUnbroken hprev.sel);
-  typst = hprev.typst_0_5_0_3;
+  typst = hprev.typst_0_5_0_4;
   typst-symbols = hprev.typst-symbols_0_1_6;
   texmath = hprev.texmath_0_12_8_9;
 }

f8548b3900a1e37c36d4b7bacc503c1ccb00127f
Magnus Viernickel <magnus.viernickel@wire.com>
2024-08-26 11:22:57 +0200

    [chore] bump nix env

--- nix/haskell-overlay.nix
+++ nix/haskell-overlay.nix
@@ -1,18 +1,21 @@
 {hlib, ...}: hfinal: hprev: {
   cabal-audit = hfinal.callPackage ./cabal-audit.nix {};
   osv = hfinal.callPackage ./osv.nix {};
-  hsec-core = hfinal.callPackage ./hsec-core.nix {};
-  hsec-tools = hfinal.callPackage ./hsec-tools.nix {};
+  hsec-core = hlib.doJailbreak (hfinal.callPackage ./hsec-core.nix {});
+  hsec-tools = hlib.doJailbreak (hfinal.callPackage ./hsec-tools.nix {});
   cvss = hfinal.callPackage ./cvss.nix {};
 
-  Cabal-syntax = hprev.Cabal-syntax_3_10_3_0;
-  Cabal = hprev.Cabal_3_10_3_0;
+  Cabal-syntax = hprev.Cabal-syntax_3_12_1_0;
+  Cabal = hprev.Cabal_3_12_1_0;
 
-  extensions = hprev.extensions.override {inherit (hfinal) Cabal;};
+  extensions = hprev.extensions_0_1_0_2.override {inherit (hfinal) Cabal;};
+
+  ormolu = hlib.doJailbreak (hprev.ormolu.override {inherit (hfinal) Cabal-syntax;});
+  fourmolu = hlib.doJailbreak (hprev.fourmolu.override {inherit (hfinal) Cabal-syntax;});
 
   toml-parser = hprev.toml-parser_2_0_1_0;
   sel = hlib.doJailbreak (hlib.markUnbroken hprev.sel);
-  typst = hprev.typst_0_5_0_4;
+  typst = hprev.typst_0_5_0_5;
   typst-symbols = hprev.typst-symbols_0_1_6;
   texmath = hprev.texmath_0_12_8_9;
 }
